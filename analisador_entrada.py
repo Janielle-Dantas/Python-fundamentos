9388efb5cc1269297618e754aced0aae7d4231b1
Janielle-Dantas <jani_dantas@hotmail.com>
2026-01-28 19:18:36 -0300

    Adiciona analisador_entrada

---
+++ analisador_entrada.py
@@ -0,0 +1,13 @@
+# Analisador de Entrada
+
+a = input("Digite algo: ")
+
+print("Análise do valor digitado:")
+print("Tipo primitivo:", type(a))
+print("Só tem espaços?", a.isspace())
+print("É um número?", a.isnumeric())
+print("É alfabético?", a.isalpha())
+print("É alfanumérico?", a.isalnum())
+print("Está em maiúsculas?", a.isupper())
+print("Está em minúsculas?", a.islower())
+
